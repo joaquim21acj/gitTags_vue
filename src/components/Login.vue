6fcd347cc14f2a474845fc734b56c7502b775202
Joaquim Alves <joaquimjunior13@gmail.com>
2020-01-19 22:56:20 -0300

    Adição da tela para digitar o username

---
+++ src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="Login">
+    <!-- <h1>{{ msg }}</h1> -->
+    <label for="basic-url">Please tipe your username from github</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon3">https://github.com/</span>
+      </div>
+      <input type="text" v-model="userLogin" class="form-control" aria-label="Username" placeholder="Username" id="user" aria-describedby="basic-addon3" />
+    </div>
+      <button type="button" class="btn btn-info" v-on:click="validateUserlogin">Get repositories</button>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }, 
+  data:{
+    userLogin: ''
+  },
+  methods: {
+    validateUserlogin(){
+      let vm = this;
+      if (vm.userLogin === null)
+        alert("Preencha o nome de usuário");
+        return;
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Login/>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+export default {
+  name: 'home',
+  components: {
+    Login
+  }
+}
+</script>
